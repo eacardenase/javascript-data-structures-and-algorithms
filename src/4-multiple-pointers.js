d4c69ca92d55827e899961fcb078560a19cbdfe3
Edwin Alexander Cardenas Esteban <eacardenase@unal.edu.co>
2022-06-22 11:36:36 -0500

    sumZero v3

--- src/4-multiple-pointers.js
+++ src/4-multiple-pointers.js
@@ -1,22 +1,39 @@
-function sumZero(sortedArray) {
-    let start = 0;
-    let end = sortedArray.length - 1;
+// function sumZero(sortedArray) {
+//     let start = 0;
+//     let end = sortedArray.length - 1;
 
-    for (let i = 0; i < sortedArray.length; i++) {
-        if (start === end) {
-            break;
-        }
+//     for (let i = 0; i < sortedArray.length; i++) {
+//         if (start === end) {
+//             break;
+//         }
 
-        if (sortedArray[start] + sortedArray[end] === 0) {
-            return [sortedArray[start], sortedArray[end]];
-        } else if (sortedArray[start] + sortedArray[end] > 0) {
-            end--;
+//         if (sortedArray[start] + sortedArray[end] === 0) {
+//             return [sortedArray[start], sortedArray[end]];
+//         } else if (sortedArray[start] + sortedArray[end] > 0) {
+//             end--;
+//         } else {
+//             start++;
+//         }
+//     }
+
+//     return undefined;
+// }
+
+function sumZero(arr) {
+    let left = 0;
+    let right = arr.length - 1;
+
+    while (left < right) {
+        let sum = arr[left] + arr[right];
+
+        if (sum === 0) {
+            return [arr[left], arr[right]];
+        } else if (sum > 0) {
+            right--;
         } else {
-            start++;
+            left++;
         }
     }
-
-    return undefined;
 }
 
 console.log(sumZero([-3, -2, -1, 0, 1, 2, 3])); // [-3, 3]
