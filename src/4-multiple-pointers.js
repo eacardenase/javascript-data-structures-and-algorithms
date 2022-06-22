df069a9e75e178cfb1ef89c3da79b9e28db3ef3a
Edwin Alexander Cardenas Esteban <eacardenase@unal.edu.co>
2022-06-22 11:22:09 -0500

    sumZero v2

--- src/4-multiple-pointers.js
+++ src/4-multiple-pointers.js
@@ -1,19 +1,21 @@
 function sumZero(sortedArray) {
-    let start = 0;
+    let start = sortedArray[0];
     let end = sortedArray.length - 1;
 
     for (let i = 0; i < sortedArray.length; i++) {
         if (sortedArray[start] + sortedArray[end] === 0) {
             return [sortedArray[start], sortedArray[end]];
+        } else if (sortedArray[start] + sortedArray[end] > 0) {
+            end--;
+        } else {
+            start++;
         }
-
-        start++;
-        end--;
     }
 
     return undefined;
 }
 
-console.log(sumZero([-3, -2, -1, 0, 1, 2, 3]));
+console.log(sumZero([-3, -2, -1, 0, 1, 2, 3])); // [-3, 3]
+console.log(sumZero([-4, -3, -2, -1, 0, 1, 2, 5])); // [-2, 2]
 console.log(sumZero([-2, 0, 1, 3]));
 console.log(sumZero([1, 2, 3]));
