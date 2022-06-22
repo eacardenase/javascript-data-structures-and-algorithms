cad4d5fd9ecef85c3b59d9df289a099e69f53849
Edwin Alexander Cardenas Esteban <eacardenase@unal.edu.co>
2022-06-22 11:30:52 -0500

    sumZero v2 fix

--- src/4-multiple-pointers.js
+++ src/4-multiple-pointers.js
@@ -1,8 +1,12 @@
 function sumZero(sortedArray) {
-    let start = sortedArray[0];
+    let start = 0;
     let end = sortedArray.length - 1;
 
     for (let i = 0; i < sortedArray.length; i++) {
+        if (start === end) {
+            break;
+        }
+
         if (sortedArray[start] + sortedArray[end] === 0) {
             return [sortedArray[start], sortedArray[end]];
         } else if (sortedArray[start] + sortedArray[end] > 0) {
@@ -17,5 +21,5 @@ function sumZero(sortedArray) {
 
 console.log(sumZero([-3, -2, -1, 0, 1, 2, 3])); // [-3, 3]
 console.log(sumZero([-4, -3, -2, -1, 0, 1, 2, 5])); // [-2, 2]
-console.log(sumZero([-2, 0, 1, 3]));
-console.log(sumZero([1, 2, 3]));
+console.log(sumZero([-2, 0, 1, 3])); // undefined
+console.log(sumZero([1, 2, 3])); // undefined
