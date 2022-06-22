65385fd0192fa58ff490fa5e53085d1c9d7a9f11
Edwin Alexander Cardenas Esteban <eacardenase@unal.edu.co>
2022-06-22 14:35:44 -0500

    countUniqueValues v1

--- src/4-multiple-pointers.js
+++ src/4-multiple-pointers.js
@@ -36,7 +36,24 @@ function sumZero(arr) {
     }
 }
 
-console.log(sumZero([-3, -2, -1, 0, 1, 2, 3])); // [-3, 3]
-console.log(sumZero([-4, -3, -2, -1, 0, 1, 2, 5])); // [-2, 2]
-console.log(sumZero([-2, 0, 1, 3])); // undefined
-console.log(sumZero([1, 2, 3])); // undefined
+// console.log(sumZero([-3, -2, -1, 0, 1, 2, 3])); // [-3, 3]
+// console.log(sumZero([-4, -3, -2, -1, 0, 1, 2, 5])); // [-2, 2]
+// console.log(sumZero([-2, 0, 1, 3])); // undefined
+// console.log(sumZero([1, 2, 3])); // undefined
+
+function countUniqueValues(sortedArray) {
+    let count = sortedArray.length > 0 ? 1 : 0;
+
+    for (let i = 1; i < sortedArray.length; i++) {
+        if (sortedArray[i] !== sortedArray[i - 1]) {
+            count++;
+        }
+    }
+
+    return count;
+}
+
+console.log(countUniqueValues([1, 1, 1, 1, 1, 2]));
+console.log(countUniqueValues([1, 2, 3, 4, 4, 4, 7, 7, 12, 12, 13]));
+console.log(countUniqueValues([]));
+console.log(countUniqueValues([-2, -1, -1, 0, 1]));
