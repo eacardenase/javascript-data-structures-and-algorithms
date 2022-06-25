46bfec6bbb66deffce540e5934193f36ff9fba45
Edwin Alexander Cardenas Esteban <eacardenase@unal.edu.co>
2022-06-25 00:18:26 -0500

    optional challenge 6 with multiple pointers

--- src/4-multiple-pointers.js
+++ src/4-multiple-pointers.js
@@ -151,7 +151,38 @@ function averagePair(array, average) {
     return false;
 }
 
-console.log(averagePair([1, 2, 3], 2.5)); // true
-console.log(averagePair([1, 3, 3, 5, 6, 7, 10, 12, 19], 8)); // true
-console.log(averagePair([-1, 0, 3, 4, 5, 6], 4.1)); // false
-console.log(averagePair([], 4)); // false
+// console.log(averagePair([1, 2, 3], 2.5)); // true
+// console.log(averagePair([1, 3, 3, 5, 6, 7, 10, 12, 19], 8)); // true
+// console.log(averagePair([-1, 0, 3, 4, 5, 6], 4.1)); // false
+// console.log(averagePair([], 4)); // false
+
+/**
+ *
+ * @param {string} str1
+ * @param {string} str2
+ * @return {boolean}
+ */
+function isSubsequence(str1, str2) {
+    let left = 0;
+    let right = 0;
+
+    while (right < str2.length - 1) {
+        if (str1[left] === str2[right]) {
+            left++;
+            right++;
+        } else if (str1[left] !== str2[right]) {
+            right++;
+        }
+    }
+
+    if (left < str1.length - 1) {
+        return false;
+    }
+
+    return true;
+}
+
+console.log(isSubsequence('hello', 'hello world')); // true
+console.log(isSubsequence('sing', 'sting')); // true
+console.log(isSubsequence('abc', 'abracadabra')); // true
+console.log(isSubsequence('abc', 'acb')); // false
