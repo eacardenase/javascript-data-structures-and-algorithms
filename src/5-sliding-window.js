06c0c5d3f3c5aff6710cff7302d79eef574a462a
Edwin Alexander Cardenas Esteban <eacardenase@unal.edu.co>
2022-06-26 13:16:06 -0500

    optional challenge 9 v1

--- src/5-sliding-window.js
+++ src/5-sliding-window.js
@@ -91,10 +91,44 @@ function minSubArrayLen(array, target) {
     return minLen === Infinity ? 0 : minLen;
 }
 
-console.log(minSubArrayLen([2, 3, 1, 2, 4, 3], 7)); // 2
-console.log(minSubArrayLen([2, 1, 6, 5, 4], 9)); // 2
-console.log(minSubArrayLen([3, 1, 7, 11, 2, 9, 8, 21, 62, 33, 19], 52)); // 1
-console.log(minSubArrayLen([1, 4, 16, 22, 5, 7, 8, 9, 10], 39)); // 3
-console.log(minSubArrayLen([1, 4, 16, 22, 5, 7, 8, 9, 10], 55)); // 5
-console.log(minSubArrayLen([4, 3, 3, 8, 1, 2, 3], 11)); // 2
-console.log(minSubArrayLen([1, 4, 16, 22, 5, 7, 8, 9, 10], 95)); // 0
+// console.log(minSubArrayLen([2, 3, 1, 2, 4, 3], 7)); // 2
+// console.log(minSubArrayLen([2, 1, 6, 5, 4], 9)); // 2
+// console.log(minSubArrayLen([3, 1, 7, 11, 2, 9, 8, 21, 62, 33, 19], 52)); // 1
+// console.log(minSubArrayLen([1, 4, 16, 22, 5, 7, 8, 9, 10], 39)); // 3
+// console.log(minSubArrayLen([1, 4, 16, 22, 5, 7, 8, 9, 10], 55)); // 5
+// console.log(minSubArrayLen([4, 3, 3, 8, 1, 2, 3], 11)); // 2
+// console.log(minSubArrayLen([1, 4, 16, 22, 5, 7, 8, 9, 10], 95)); // 0
+
+/**
+ *
+ * @param {string} word
+ * @return {number}
+ */
+function findLongestSubstring(word) {
+    let longest = 0;
+    let seen = {};
+    let start = 0;
+
+    for (let i = 0; i < word.length; i++) {
+        console.log(seen);
+
+        let char = word[i];
+
+        if (seen[char]) {
+            start = Math.max(start, seen[char]);
+        }
+
+        longest = Math.max(longest, i - start + 1);
+        seen[char] = i + 1;
+    }
+
+    return longest;
+}
+
+// console.log(findLongestSubstring('')); // 0
+console.log(findLongestSubstring('rithmschool')); // 7
+// console.log(findLongestSubstring('thisisawesome')); // 6
+// console.log(findLongestSubstring('thecantinthehat')); // 7
+// console.log(findLongestSubstring('bbbbbb')); // 1
+// console.log(findLongestSubstring('longestsubstring')); // 8
+// console.log(findLongestSubstring('thisishowwedoit')); // 6
