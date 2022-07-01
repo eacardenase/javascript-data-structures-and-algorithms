461cd50007ab7477368d95f7603af6e86f550cdb
Edwin Alexander Cardenas Esteban <eacardenase@unal.edu.co>
2022-07-01 10:10:40 -0500

    challenge 21

--- src/7-recursion.js
+++ src/7-recursion.js
@@ -354,4 +354,56 @@ function capitalizeFirst(arr) {
     return newArray.concat(capitalizeFirst(arr.slice(1)));
 }
 
-console.log(capitalizeFirst(['car', 'taco', 'banana'])); // ['Car','Taco','Banana']
+// console.log(capitalizeFirst(['car', 'taco', 'banana'])); // ['Car','Taco','Banana']
+
+/**
+ *
+ * @param {Object} obj
+ * @returns {number}
+ */
+function nestedEvenSum(obj) {}
+
+var obj1 = {
+    outer: 2,
+    obj: {
+        inner: 2,
+        otherObj: {
+            superInner: 2,
+            notANumber: true,
+            alsoNotANumber: 'yup',
+        },
+    },
+};
+
+var obj2 = {
+    a: 2,
+    b: { b: 2, bb: { b: 3, bb: { b: 2 } } },
+    c: { c: { c: 2 }, cc: 'ball', ccc: 5 },
+    d: 1,
+    e: { e: { e: 2 }, ee: 'car' },
+};
+
+// console.log(nestedEvenSum(obj1)); // 6
+// console.log(nestedEvenSum(obj2)); // 10
+
+/**
+ *
+ * @param {string[]} strArray
+ * @returns {string[]}
+ */
+function capitalizeWords(strArray) {
+    let newArray = [];
+
+    if (strArray.length === 0) {
+        return newArray;
+    }
+
+    let currentEl = strArray[0];
+
+    newArray.push(currentEl.toUpperCase());
+
+    return newArray.concat(capitalizeWords(strArray.slice(1)));
+}
+
+let words = ['i', 'am', 'learning', 'recursion'];
+console.log(capitalizeWords(words)); // ['I', 'AM', 'LEARNING', 'RECURSION']
