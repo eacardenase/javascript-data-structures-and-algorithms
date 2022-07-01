b0901737f5e66eaf24ed04b79b3472e7985152f9
Edwin Alexander Cardenas Esteban <eacardenase@unal.edu.co>
2022-07-01 15:11:22 -0500

    challenge 20

--- src/7-recursion.js
+++ src/7-recursion.js
@@ -329,10 +329,10 @@ function someRecursive(arr, cb) {
     return someRecursive(arr.slice(1), cb);
 }
 
-console.log(someRecursive([1, 2, 3, 4], isOdd)); // true
-console.log(someRecursive([4, 6, 8, 9], isOdd)); // true
-console.log(someRecursive([4, 6, 8], isOdd)); // false
-console.log(someRecursive([4, 6, 8], (val) => val > 10)); // false
+// console.log(someRecursive([1, 2, 3, 4], isOdd)); // true
+// console.log(someRecursive([4, 6, 8, 9], isOdd)); // true
+// console.log(someRecursive([4, 6, 8], isOdd)); // false
+// console.log(someRecursive([4, 6, 8], (val) => val > 10)); // false
 
 /**
  *
@@ -387,35 +387,45 @@ function capitalizeFirst(arr) {
 
 // console.log(capitalizeFirst(['car', 'taco', 'banana'])); // ['Car','Taco','Banana']
 
-// /**
-//  *
-//  * @param {Object} obj
-//  * @returns {number}
-//  */
-// function nestedEvenSum(obj) {}
+/**
+ *
+ * @param {Object} obj
+ * @returns {number}
+ */
+function nestedEvenSum(obj, sum = 0) {
+    for (let key in obj) {
+        if (typeof obj[key] === 'object') {
+            sum += nestedEvenSum(obj[key]);
+        } else if (typeof obj[key] === 'number' && obj[key] % 2 === 0) {
+            sum += obj[key];
+        }
+    }
 
-// var obj1 = {
-//     outer: 2,
-//     obj: {
-//         inner: 2,
-//         otherObj: {
-//             superInner: 2,
-//             notANumber: true,
-//             alsoNotANumber: 'yup',
-//         },
-//     },
-// };
+    return sum;
+}
 
-// var obj2 = {
-//     a: 2,
-//     b: { b: 2, bb: { b: 3, bb: { b: 2 } } },
-//     c: { c: { c: 2 }, cc: 'ball', ccc: 5 },
-//     d: 1,
-//     e: { e: { e: 2 }, ee: 'car' },
-// };
+var obj1 = {
+    outer: 2,
+    obj: {
+        inner: 2,
+        otherObj: {
+            superInner: 2,
+            notANumber: true,
+            alsoNotANumber: 'yup',
+        },
+    },
+};
 
-// console.log(nestedEvenSum(obj1)); // 6
-// console.log(nestedEvenSum(obj2)); // 10
+var obj2 = {
+    a: 2,
+    b: { b: 2, bb: { b: 3, bb: { b: 2 } } },
+    c: { c: { c: 2 }, cc: 'ball', ccc: 5 },
+    d: 1,
+    e: { e: { e: 2 }, ee: 'car' },
+};
+
+console.log(nestedEvenSum(obj1)); // 6
+console.log(nestedEvenSum(obj2)); // 10
 
 /**
  *
