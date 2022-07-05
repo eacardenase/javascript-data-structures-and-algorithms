f3747c16551e12f314a5268e485637502fdd00fa
Edwin Alexander Cardenas Esteban <eacardenase@unal.edu.co>
2022-07-05 11:38:35 -0500

    string search v1

--- src/8-searching-algorithms.js
+++ src/8-searching-algorithms.js
@@ -44,8 +44,8 @@ function binarySearch(numberArr, targetNum) {
     return -1;
 }
 
-console.log(binarySearch([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], 6)); // 6
-console.log(binarySearch([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], 10)); // -1
+// console.log(binarySearch([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], 6)); // 6
+// console.log(binarySearch([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], 10)); // -1
 // console.log(
 //     binarySearch([0, 0, 0, 1, 2, 3, 3, 3, 4, 5, 5, 5, 6, 7, 7, 8, 9], 4)
 // ); // 8
@@ -53,3 +53,30 @@ console.log(binarySearch([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], 10)); // -1
 //     binarySearch([0, 0, 0, 1, 2, 3, 3, 3, 4, 5, 5, 5, 6, 7, 7, 8, 9], 9)
 // ); // 16
 // console.log(binarySearch([1, 2, 3, 4, 5], 5)); // 4
+
+/**
+ *
+ * @param {string} str1
+ * @param {string} str2
+ * @returns {number}
+ */
+function naiveStrSearch(str1, str2) {
+    let count = 0;
+    let index = 0;
+
+    for (let i = 0; i <= str1.length; i++) {
+        if (str1[i] === str2[index]) {
+            index++;
+            if (index === str2.length) {
+                count++;
+                index = 0;
+            }
+        } else {
+            index = 0;
+        }
+    }
+
+    return count;
+}
+
+console.log(naiveStrSearch('wowomgzomgomg', 'omg'));
