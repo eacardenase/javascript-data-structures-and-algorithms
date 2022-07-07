0b2229d29fca4340a1170a18285c5317e3ccf08f
Edwin Alexander Cardenas Esteban <eacardenase@unal.edu.co>
2022-07-06 20:19:04 -0500

    bubble sort optimized when the array is nearly sorted

--- src/9-sorting-algorithms.js
+++ src/9-sorting-algorithms.js
@@ -21,18 +21,28 @@ function swap(arr, idx1, idx2) {
 function bubbleSort(array) {
     const sortedArray = [...array];
     let end = sortedArray.length - 1;
+    let noSwaps = false;
 
     for (let i = end; i >= 0; i--) {
+        noSwaps = true;
+
         for (let j = 0; j <= i - 1; j++) {
             if (sortedArray[j] > sortedArray[j + 1]) {
                 let temp = sortedArray[j];
                 sortedArray[j] = sortedArray[j + 1];
                 sortedArray[j + 1] = temp;
+                noSwaps = false;
             }
+        }
+
+        if (noSwaps) {
+            break;
         }
     }
 
     return sortedArray;
 }
 
-console.log(bubbleSort([29, 10, 14, 30, 37, 14, 18])); // [10, 14, 14, 18, 29, 30, 37]
+// console.log(bubbleSort([29, 10, 14, 30, 37, 14, 18])); // [10, 14, 14, 18, 29, 30, 37]
+// console.log(bubbleSort([37, 45, 29, 8, 12, 88, -3])); // [-3, 8, 12, 29, 37, 45, 88]
+console.log(bubbleSort([8, 1, 2, 3, 4, 5, 6, 7]));
